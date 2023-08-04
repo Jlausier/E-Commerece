8b92767846e4f3d6bc77ea725466cf0c0d05581f
Jacob <jakelausier7878@gmail.com>
2023-08-04 10:54:02 -0400

    fixed all the models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,26 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Product', // Replace 'Product' with the actual name of the Product model
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Tag', // Replace 'Tag' with the actual name of the Tag model
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
